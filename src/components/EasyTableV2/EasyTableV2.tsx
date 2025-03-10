ad86b03b66195ff227c3255b0246c908809284a2
Maxime Brunet <maximb84@gmail.com>
2025-03-10 17:10:43 +0100

    Update for npm publish

---
+++ src/components/EasyTableV2/EasyTableV2.tsx
@@ -0,0 +1,5 @@
+const EasyTableV2 = () => {
+  return <div>Hello, World! This is easyv2-table!</div>;
+};
+
+export default EasyTableV2;
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { default as EasyTableV2 } from "./components/EasyTableV2/EasyTableV2";
