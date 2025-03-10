3a8a4cfc1ff8423ed988f089055c80d327e5affb
Maxime Brunet <maximb84@gmail.com>
2025-03-10 17:16:19 +0100

    chore: finalize features before version bump

--- src/components/EasyTableV2/EasyTableV2.tsx
+++ src/components/EasyTableV2/EasyTableV2.tsx
@@ -1,5 +1,34 @@
-const EasyTableV2 = () => {
-  return <div>Hello, World! This is easyv2-table!</div>;
-};
+export interface ColumnDef<T> {
+  key: keyof T;
+  label: string;
+}
+
+interface EasyTableProps<T> {
+  data: T[];
+  columns: ColumnDef<T>[];
+}
+
+function EasyTableV2<T extends { id: string }>({ data, columns }: EasyTableProps<T>) {
+  return (
+    <table border={1}>
+      <thead>
+        <tr>
+          {columns.map((col) => (
+            <th key={String(col.key)}>{col.label}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            {columns.map((col) => (
+              <td key={String(col.key)}>{String(item[col.key])}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
 
 export default EasyTableV2;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { default as EasyTableV2 } from "./components/EasyTableV2/EasyTableV2";
+export type { ColumnDef } from "./components/EasyTableV2/EasyTableV2";
